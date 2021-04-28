9304a818d6cbad1f657267da84b81dc5e1e80637
Thomas Wright <twwright@gmail.com>
2021-04-27 23:38:59 -0500

    Implement simple JSON DB to record form submissions

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +10,10 @@ def home():
 @app.route('/short-url', methods=['GET','POST'])
 def short_url():
   if request.method == 'POST':
+    urls = {}
+    urls[request.form['code']] = {'url':request.form['url']}
+    with open('urls.json', 'w') as url_file:
+      json.dump(urls, url_file)
     return render_template('short_url.html', code = request.form['code'])
   else:
     return redirect(url_for('home'))
