741025fbc03c60210ba43b1a7da6a615f03f9322
Thomas Wright <twwright@gmail.com>
2021-04-28 00:09:41 -0500

    Implement file storage from POST submission

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = "crossfit"
@@ -22,7 +23,14 @@ def short_url():
       flash('Sorry, that short name has already been taken. Try again.')
       return redirect(url_for('home'))
 
-    urls[request.form['code']] = {'url':request.form['url']}
+    if 'url' in request.form.keys():
+      urls[request.form['code']] = {'url':request.form['url']}
+    else:
+      f = request.files['file']
+      full_name = request.form['code'] + secure_filename(f.filename)
+      f.save('/Users/thomaswright/LearnDev/shorter-url/' + full_name)
+      urls[request.form['code']] = {'file':full_name}
+
     with open('urls.json', 'w') as url_file:
       json.dump(urls, url_file)
     return render_template('short_url.html', code = request.form['code'])
